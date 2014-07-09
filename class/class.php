73dfbcfee0cf5057be9ada7b12b80354eadc8f50
Антон Кондратов <contact@site-home.ru>
2014-07-09 22:38:51 +0400

    Увеличил скорость готовности страницы, поправил кодестайл

--- class/class.php
+++ class/class.php
@@ -1,87 +1,114 @@
-<?
+<?php
 class Pwd {
 
-	/**
-	 * Функция вернёт пароль с длинной $length,
-	 * если $checkForNumbers == true, то в пароле обязательно будет цифра
-	 * если $checkForNumbers == false, то в пароле не будет цифр
-	 * если $checkForMarks == true, то в пароле обязательно будет знак препинания
-	 * если $checkForMarks == false, то в пароле обязательно не будет знаков препинания
-	 */
-	function create_and_check($length, $checkForNumbers, $checkForMarks) {
-		$symbols = array();
-		$symbols['letters'] = array(
-			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z',
-			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z',
-		);
-		$symbols['numbers'] = array(
-			'1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-		);
-		$symbols['marks'] = array(
-			'.', ',', '-', '!', '(', ')',
-			// '[', ']', '?', '&', '^', '%', '@', '*', '$', '<', '>', '/', '|', '+', '{', '}', '`', '~'
-		);
-		$symbols['all'] = array_merge(
-			$symbols['letters'],
-			$checkForNumbers ? $symbols['numbers'] : array(),
-			$checkForMarks ? $symbols['marks'] : array()
-		);
+    /**
+     * Функция вернёт пароль с длинной $length,
+     * если $checkForNumbers == true, то в пароле обязательно будет цифра
+     * если $checkForNumbers == false, то в пароле не будет цифр
+     * если $checkForMarks == true, то в пароле обязательно будет знак препинания
+     * если $checkForMarks == false, то в пароле обязательно не будет знаков препинания
+     * если $checkForExtra == true, то в пароле обязательно будет страшный знак препинания
+     * если $checkForExtra == false, то в пароле обязательно не будет страшных знаков препинания
+     */
+    function createAndCheck($length, $checkForNumbers, $checkForMarks, $checkForExtra) {
+        $symbols = array();
+        $symbols["letters"] = array(
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+            "r", "s", "t", "u", "v", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H",
+            "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "X", "Y", "Z",
+        );
+        $symbols["numbers"] = array(
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        );
+        $symbols["marks"] = array(
+            ".", ",", "+", "-", "_", "!", "@",
+        );
+        $symbols["extra"] = array(
+            "(", ")", "[", "]", "{", "}", "?", "&", "^", "%", "*", "$", "/", "|", "`", "~"
+        );
+        $symbols["all"] = array_merge(
+            $symbols["letters"],
+            $checkForNumbers ? $symbols["numbers"] : array(),
+            $checkForMarks ? $symbols["marks"] : array(),
+            $checkForExtra ? $symbols["extra"] : array()
+        );
 
-		$pwd = null;
+        $pwd = null;
 
-		while (
-			strlen($pwd) == 0
-			|| ($checkForNumbers && (
-					!isset($hasNumber)
-					|| !$hasNumber
-				)
-			) // есть ли цифры
-			|| ($checkForMarks && (
-					!isset($hasNumber)
-					|| !$hasMark
-				)
-			) // есть ли символы
-		) {
-			$hasNumber = false;
-			$hasMark = false;
-			$pwd = self::create_password($length, $symbols);
-			if ($checkForNumbers) {
-				foreach ($symbols['numbers'] as $value) {
-					if (strpos($pwd, $value) !== false) {
-						$hasNumber = true;
-					}
-				}
-			}
-			if ($checkForMarks) {
-				foreach ($symbols['marks'] as $value) {
-					if (strpos($pwd, $value) !== false) {
-						$hasMark = true;
-					}
-				}
-			}
-		}
-		return $pwd;
-	}
+        while (
+
+            strlen($pwd) == 0
+
+            // есть ли цифры
+            || ($checkForNumbers && (
+                    !isset($hasNumber)
+                    || !$hasNumber
+                )
+            )
+
+            // есть ли символы
+            || ($checkForMarks && (
+                    !isset($hasMark)
+                    || !$hasMark
+                )
+            )
+
+            // есть ли страшные символы
+            || ($checkForExtra && (
+                    !isset($hasExtra)
+                    || !$hasExtra
+                )
+            )
+
+        ) {
+            $hasNumber = false;
+            $hasMark = false;
+            $hasExtra = false;
+            $pwd = self::createPassword($length, $symbols);
+            if ($checkForNumbers) {
+                foreach ($symbols["numbers"] as $value) {
+                    if (strpos($pwd, $value) !== false) {
+                        $hasNumber = true;
+                    }
+                }
+            }
+            if ($checkForMarks) {
+                foreach ($symbols["marks"] as $value) {
+                    if (strpos($pwd, $value) !== false) {
+                        $hasMark = true;
+                    }
+                }
+            }
+            if ($checkForExtra) {
+                foreach ($symbols["extra"] as $value) {
+                    if (strpos($pwd, $value) !== false) {
+                        $hasExtra = true;
+                    }
+                }
+            }
+        }
+        return $pwd;
+    }
 
 
-	/**
-	 * Функция генерирует пароль
-	 */
-	function create_password($length, $symbols) {
-		$pwd = null;
-		$countAll = count($symbols['all']) - 1;
-		$countS = count($symbols['letters']) - 1;
-		for ($i = 0; $i < $length; $i++) {
-			if ($i == 0) {
-				$pwd .= $symbols['letters'][rand(0, $countS)];
-			} else if ($i == $length - 1) {
-				$pwd .= $symbols['letters'][rand(0, $countS)];
-			} else {
-				$pwd .= $symbols['all'][rand(0, $countAll)];
-			}
-		}
-		return $pwd;
-	}
+    /**
+     * Функция генерирует пароль
+     */
+    function createPassword($length, $symbols) {
+        $pwd = null;
+        $countAll = count($symbols["all"]) - 1;
+        $countS = count($symbols["letters"]) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            if ($i == 0) {
+                $pwd .= $symbols["letters"][rand(0, $countS)];
+            } else if ($i == $length - 1) {
+                $pwd .= $symbols["letters"][rand(0, $countS)];
+            } else {
+                $pwd .= $symbols["all"][rand(0, $countAll)];
+            }
+        }
+        return $pwd;
+    }
 
 }
 ?>
--- js/scripts.js
+++ js/scripts.js
@@ -1,115 +1,115 @@
 ;(function($) {
-	$(document).ready(function() {
+    $(document).ready(function() {
 
-		/**
-		 * Проверяем наличие флеша
-		 */
+        /**
+         * Проверяем наличие флеша
+         */
 
-		if ( navigator.plugins["Shockwave Flash"] ) {
-			$('html').removeClass('no-shockwave-flash').addClass('shockwave-flash');
-		}
-
-
-		/**
-		 * Определяем браузер
-		 */
-
-		var browser;
-		if ( $.browser.webkit || $.browser.safari ) {
-			browser = "webkit";
-		} else if ( $.browser.opera ) {
-			browser = "opera";
-		} else if ( $.browser.msie ) {
-			browser = "ie";
-		} else if ( $.browser.mozilla ) {
-			browser = "mozilla";
-		}
+        if (navigator.plugins["Shockwave Flash"]) {
+            $("html").removeClass("no-shockwave-flash").addClass("shockwave-flash");
+        }
 
 
-		/**
-		 * Функция показывает noty
-		 */
+        /**
+         * Определяем браузер
+         */
 
-		// флаг факта открытия хотя бы одного уведомления
-		var has_noty = false;
-
-		$.fn.alert = function( pwd ) {
-			if ( typeof pwd == "undefined" ) {
-				text = 'Пароль скопирован';
-			} else {
-				text = 'Скопирован пароль ' + pwd;
-			}
-			if ( !$.browser.mozilla ) {
-				noty({
-					layout: 'topCenter',
-					type: 'success',
-					text: text,
-					timeout: 1500,
-					maxVisible: 3,
-					animation: {
-						open: {height: 'toggle'},
-						close: {height: 'toggle'},
-						easing: 'swing',
-						speed: 100
-					}
-				});
-				has_noty = true;
-			}
-		};
+        var browser;
+        if ($.browser.webkit || $.browser.safari) {
+            browser = "webkit";
+        } else if ($.browser.opera) {
+            browser = "opera";
+        } else if ($.browser.msie) {
+            browser = "ie";
+        } else if ($.browser.mozilla) {
+            browser = "mozilla";
+        }
 
 
-		/**
-		 * Если это не старые IE и включен флеш, то вешаем обработку копирования в буфер
-		 * и уведомление об этом
-		 */
-		if ( !$('html').hasClass('lt-ie9') && navigator.plugins["Shockwave Flash"] ) {
+        /**
+         * Функция показывает noty
+         */
 
-			var pwds = $('.pwd');
-			pwds.each(function(i, e) {
+        // флаг факта открытия хотя бы одного уведомления
+        var has_noty = false;
 
-				var pwd = $(e).text();
-				$(e).zclip({
-					path: 'js/vendor/jquery.zclip.1.1.1/ZeroClipboard.swf',
-					copy: function() {
-						return pwd;
-					},
-					beforeCopy: function() {
-						pwds.removeClass('copied');
-					},
-					afterCopy: function() {
+        $.fn.alert = function(pwd) {
+            if (typeof pwd == "undefined") {
+                text = "Пароль скопирован";
+            } else {
+                text = "Скопирован пароль " + pwd;
+            }
+            if (!$.browser.mozilla) {
+                noty({
+                    layout: "topCenter",
+                    type: "success",
+                    text: text,
+                    timeout: 1500,
+                    maxVisible: 3,
+                    animation: {
+                        open: {height: "toggle"},
+                        close: {height: "toggle"},
+                        easing: "swing",
+                        speed: 100
+                    }
+                });
+                has_noty = true;
+            }
+        };
 
-						$(this).addClass('copied');
 
-						$.fn.alert( pwd );
+        /**
+         * Если это не старые IE и включен флеш, то вешаем обработку копирования в буфер
+         * и уведомление об этом
+         */
+        if (!$("html").hasClass("lt-ie9") && navigator.plugins["Shockwave Flash"]) {
 
-					}
-				});
+            var pwds = $(".pwd");
+            pwds.each(function(i, e) {
 
-			});
+                var pwd = $(e).text();
+                $(e).zclip({
+                    path: "js/vendor/jquery.zclip.1.1.1/ZeroClipboard.swf",
+                    copy: function() {
+                        return pwd;
+                    },
+                    beforeCopy: function() {
+                        pwds.removeClass("copied");
+                    },
+                    afterCopy: function() {
 
-		}
+                        $(this).addClass("copied");
+
+                        $.fn.alert(pwd);
+
+                    }
+                });
+
+            });
+
+        }
 
 
-		/**
-		 * Обновляем страницу по клику на кнопку
-		 */
+        /**
+         * Обновляем страницу по клику на кнопку
+         */
 
-		$('.refresh').eq(0).on('click', function(event) {
+        $(".refresh").eq(0).on("click", function(event) {
 
-			event.preventDefault();
+            event.preventDefault();
 
-			// Скрываем уведомления, если они были
-			if ( has_noty ) {
-				$.noty.closeAll();
-			}
+            // Скрываем уведомления, если они были
+            if (has_noty) {
+                $.noty.closeAll();
+            }
 
-			$(this).addClass('loader');
+            $(this).addClass("loader");
 
-			setTimeout(function() {
-				location.reload();
-			}, 115);
+            setTimeout(function() {
+                location.reload();
+            }, 115);
 
-		});
+        });
 
-	});
+    });
 })(jQuery);
