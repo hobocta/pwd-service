a8e108dff670368797c312fb38ef192c753af27a
Anton Kondratov <contact@site-home.ru>
2015-03-03 01:21:05 +0300

    Табы и убрал лишнее

--- class/class.php
+++ class/class.php
@@ -1,114 +1,114 @@
 <?php
 class Pwd {
 
-    /**
-     * Функция вернёт пароль с длинной $length,
-     * если $checkForNumbers == true, то в пароле обязательно будет цифра
-     * если $checkForNumbers == false, то в пароле не будет цифр
-     * если $checkForMarks == true, то в пароле обязательно будет знак препинания
-     * если $checkForMarks == false, то в пароле обязательно не будет знаков препинания
-     * если $checkForExtra == true, то в пароле обязательно будет страшный знак препинания
-     * если $checkForExtra == false, то в пароле обязательно не будет страшных знаков препинания
-     */
-    function createAndCheck($length, $checkForNumbers, $checkForMarks, $checkForExtra) {
-        $symbols = array();
-        $symbols["letters"] = array(
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-            "r", "s", "t", "u", "v", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H",
-            "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "X", "Y", "Z",
-        );
-        $symbols["numbers"] = array(
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-        );
-        $symbols["marks"] = array(
-            ".", ",", "+", "-", "_", "!", "@",
-        );
-        $symbols["extra"] = array(
-            "(", ")", "[", "]", "{", "}", "?", "&", "^", "%", "*", "$", "/", "|", "`", "~"
-        );
-        $symbols["all"] = array_merge(
-            $symbols["letters"],
-            $checkForNumbers ? $symbols["numbers"] : array(),
-            $checkForMarks ? $symbols["marks"] : array(),
-            $checkForExtra ? $symbols["extra"] : array()
-        );
+	/**
+	 * Функция вернёт пароль с длинной $length,
+	 * если $checkForNumbers == true, то в пароле обязательно будет цифра
+	 * если $checkForNumbers == false, то в пароле не будет цифр
+	 * если $checkForMarks == true, то в пароле обязательно будет знак препинания
+	 * если $checkForMarks == false, то в пароле обязательно не будет знаков препинания
+	 * если $checkForExtra == true, то в пароле обязательно будет страшный знак препинания
+	 * если $checkForExtra == false, то в пароле обязательно не будет страшных знаков препинания
+	 */
+	function createAndCheck($length, $checkForNumbers, $checkForMarks, $checkForExtra) {
+		$symbols = array();
+		$symbols["letters"] = array(
+			"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+			"r", "s", "t", "u", "v", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H",
+			"I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "X", "Y", "Z",
+		);
+		$symbols["numbers"] = array(
+			"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+		);
+		$symbols["marks"] = array(
+			".", ",", "+", "-", "_", "!", "@",
+		);
+		$symbols["extra"] = array(
+			"(", ")", "[", "]", "{", "}", "?", "&", "^", "%", "*", "$", "/", "|", "`", "~"
+		);
+		$symbols["all"] = array_merge(
+			$symbols["letters"],
+			$checkForNumbers ? $symbols["numbers"] : array(),
+			$checkForMarks ? $symbols["marks"] : array(),
+			$checkForExtra ? $symbols["extra"] : array()
+		);
 
-        $pwd = null;
+		$pwd = null;
 
-        while (
+		while (
 
-            strlen($pwd) == 0
+			strlen($pwd) == 0
 
-            // есть ли цифры
-            || ($checkForNumbers && (
-                    !isset($hasNumber)
-                    || !$hasNumber
-                )
-            )
+			// есть ли цифры
+			|| ($checkForNumbers && (
+					!isset($hasNumber)
+					|| !$hasNumber
+				)
+			)
 
-            // есть ли символы
-            || ($checkForMarks && (
-                    !isset($hasMark)
-                    || !$hasMark
-                )
-            )
+			// есть ли символы
+			|| ($checkForMarks && (
+					!isset($hasMark)
+					|| !$hasMark
+				)
+			)
 
-            // есть ли страшные символы
-            || ($checkForExtra && (
-                    !isset($hasExtra)
-                    || !$hasExtra
-                )
-            )
+			// есть ли страшные символы
+			|| ($checkForExtra && (
+					!isset($hasExtra)
+					|| !$hasExtra
+				)
+			)
 
-        ) {
-            $hasNumber = false;
-            $hasMark = false;
-            $hasExtra = false;
-            $pwd = self::createPassword($length, $symbols);
-            if ($checkForNumbers) {
-                foreach ($symbols["numbers"] as $value) {
-                    if (strpos($pwd, $value) !== false) {
-                        $hasNumber = true;
-                    }
-                }
-            }
-            if ($checkForMarks) {
-                foreach ($symbols["marks"] as $value) {
-                    if (strpos($pwd, $value) !== false) {
-                        $hasMark = true;
-                    }
-                }
-            }
-            if ($checkForExtra) {
-                foreach ($symbols["extra"] as $value) {
-                    if (strpos($pwd, $value) !== false) {
-                        $hasExtra = true;
-                    }
-                }
-            }
-        }
-        return $pwd;
-    }
+		) {
+			$hasNumber = false;
+			$hasMark = false;
+			$hasExtra = false;
+			$pwd = self::createPassword($length, $symbols);
+			if ($checkForNumbers) {
+				foreach ($symbols["numbers"] as $value) {
+					if (strpos($pwd, $value) !== false) {
+						$hasNumber = true;
+					}
+				}
+			}
+			if ($checkForMarks) {
+				foreach ($symbols["marks"] as $value) {
+					if (strpos($pwd, $value) !== false) {
+						$hasMark = true;
+					}
+				}
+			}
+			if ($checkForExtra) {
+				foreach ($symbols["extra"] as $value) {
+					if (strpos($pwd, $value) !== false) {
+						$hasExtra = true;
+					}
+				}
+			}
+		}
+		return $pwd;
+	}
 
 
-    /**
-     * Функция генерирует пароль
-     */
-    function createPassword($length, $symbols) {
-        $pwd = null;
-        $countAll = count($symbols["all"]) - 1;
-        $countS = count($symbols["letters"]) - 1;
-        for ($i = 0; $i < $length; $i++) {
-            if ($i == 0) {
-                $pwd .= $symbols["letters"][rand(0, $countS)];
-            } else if ($i == $length - 1) {
-                $pwd .= $symbols["letters"][rand(0, $countS)];
-            } else {
-                $pwd .= $symbols["all"][rand(0, $countAll)];
-            }
-        }
-        return $pwd;
-    }
+	/**
+	 * Функция генерирует пароль
+	 */
+	function createPassword($length, $symbols) {
+		$pwd = null;
+		$countAll = count($symbols["all"]) - 1;
+		$countS = count($symbols["letters"]) - 1;
+		for ($i = 0; $i < $length; $i++) {
+			if ($i == 0) {
+				$pwd .= $symbols["letters"][rand(0, $countS)];
+			} else if ($i == $length - 1) {
+				$pwd .= $symbols["letters"][rand(0, $countS)];
+			} else {
+				$pwd .= $symbols["all"][rand(0, $countAll)];
+			}
+		}
+		return $pwd;
+	}
 
 }
 ?>
--- index.php
+++ index.php
@@ -5,78 +5,78 @@
 <!--[if gt IE 8]><!--> <html class="no-js no-shockwave-flash"> <!--<![endif]-->
 <head>
 
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Online генератор паролей</title>
+	<title>Online генератор паролей</title>
 
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write("<script src='js/vendor/jquery.1.10.1.min.js'><\/script>")</script>
-    <script src="js/vendor/modernizr.2.7.1.min.js"></script>
-    <script src="js/vendor/jquery-migrate-1.2.1.min.js"></script>
-    <script src="js/vendor/flash_detect_min.js"></script>
-    <script src="js/scripts.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write("<script src='js/vendor/jquery.1.10.1.min.js'><\/script>")</script>
+	<script src="js/vendor/modernizr.2.7.1.min.js"></script>
+	<script src="js/vendor/jquery-migrate-1.2.1.min.js"></script>
+	<script src="js/vendor/flash_detect_min.js"></script>
+	<script src="js/scripts.js"></script>
 
 </head>
 
 <body>
 
-    <div class="wrap">
+	<div class="wrap">
 
-        <p class="note">Клик на пароль скопирует его в буфер</p>
+		<p class="note">Клик на пароль скопирует его в буфер</p>
 
-        <?php
-        if (!include("class/class.php") ) {
-            die("Не удалось подгрузить класс.");
-        };
-        $Pwd = new Pwd;
-        foreach (array(8, 12, 16) as $length) {
-            ?>
-            <h3><?= $length ?></h3>
-            <?
-            foreach (
-                array(
-                    array("num" => true, "marks" => false, "extra" => false),
-                    array("num" => true, "marks" => true,  "extra" => false),
-                    array("num" => true, "marks" => true,  "extra" => true),
-                ) as $method
-            ) {
-                ?>
-                <p>
-                    <span class="pwd"><?
-                       echo $Pwd->createAndCheck(
-                          $length,
-                          $method["num"],
-                          $method["marks"],
-                          $method["extra"]
-                       );
-                    ?></span>
-                </p>
-                <?
-            }
-        }
-        ?>
+		<?php
+		if (!include("class/class.php") ) {
+			die("Не удалось подгрузить класс.");
+		};
+		$Pwd = new Pwd;
+		foreach (array(8, 12, 16) as $length) {
+			?>
+			<h3><?= $length ?></h3>
+			<?
+			foreach (
+				array(
+					array("num" => true, "marks" => false, "extra" => false),
+					array("num" => true, "marks" => true,  "extra" => false),
+					array("num" => true, "marks" => true,  "extra" => true),
+				) as $method
+			) {
+				?>
+				<p>
+					<span class="pwd"><?
+						echo $Pwd->createAndCheck(
+							$length,
+							$method["num"],
+							$method["marks"],
+							$method["extra"]
+						);
+					?></span>
+				</p>
+				<?
+			}
+		}
+		?>
 
-        <a href="/" class="refresh rotate"></a>
+		<a href="/" class="refresh rotate"></a>
 
-    </div>
+	</div>
 
-    <script>
-        ($.browser.msie && $.browser.version < 9)
-        || document.write("<script src='js/vendor/jquery.zclip.1.1.1/jquery.zclip.min.js'><\/script>")
-    </script>
-    <script src="js/vendor/jquery.noty.packaged.min.js"></script>
+	<script>
+		($.browser.msie && $.browser.version < 9)
+		|| document.write("<script src='js/vendor/jquery.zclip.1.1.1/jquery.zclip.min.js'><\/script>")
+	</script>
+	<script src="js/vendor/jquery.noty.packaged.min.js"></script>
 
-    <?php
-    $filename = "more/include.php";
-    if (file_exists($filename)) {
-        include("more/include.php");
-    }
-    ?>
+	<?php
+	$filename = "more/include.php";
+	if (file_exists($filename)) {
+		include("more/include.php");
+	}
+	?>
 
 </body>
 </html>
