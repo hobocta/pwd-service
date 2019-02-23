f7a9653c6d76a1eb6fc222809903833fa27cda5d
Антон Кондратов <hobocta@gmail.com>
2019-02-23 18:34:17 +0300

    оУбрал лишнией js, обновил jquery

--- index.php
+++ index.php
@@ -43,10 +43,7 @@ require_once 'src/autoload.php';
         <?php endforeach; ?>
     </div>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write("<script src='js/vendor/jquery.1.10.2.min.js'><\/script>")</script>
-    <script src="js/vendor/modernizr.2.7.1.min.js"></script>
-    <script src="js/vendor/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/vendor/jquery.min.js"></script>
     <script src="js/vendor/jquery.noty.packaged.min.js"></script>
     <script src="js/vendor/clipboard.min.js"></script>
     <script src="js/scripts.js"></script>
