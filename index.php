c699154ec5142b4844471ae4449994c7fc5ccbfb
Антон Кондратов <hobocta@gmail.com>
2019-02-25 21:27:37 +0300

    Сделал минификацию html

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+ob_start();
+
+require 'main.php';
+
+$html = ob_get_clean();
+
+echo sanitizeOutput($html);
+
+function sanitizeOutput($buffer)
+{
+
+    $search = array(
+        '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',     // strip whitespaces before tags, except space
+        '/(\s)+/s',         // shorten multiple whitespace sequences
+        '/<!--(.|\s)*?-->/' // Remove HTML comments
+    );
+
+    $replace = array(
+        '>',
+        '<',
+        '\\1',
+        ''
+    );
+
+    $buffer = preg_replace($search, $replace, $buffer);
+
+    return $buffer;
+}
